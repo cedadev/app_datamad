0880aad895fbd3a74f4001b168eb027a79e788f9
m-haines <matthew.r.haines@gmail.com>
2024-11-27 16:34:24 +0000

    (For now) amended signal.py to use standard Haystack rather than realTimeSignalProcessor.

--- datamad2/signals.py
+++ datamad2/signals.py
@@ -15,15 +15,16 @@ from django.dispatch import receiver
 from datamad2.models import Document
 from datamad2.search_indexes import GrantIndex
 
+"""
 class HaystackSignalProcessor(RealtimeSignalProcessor):
-    """
-    Handle signals to update search index when models are saved or deleted.
+    
+    #Handle signals to update search index when models are saved or deleted.
 
-    This is just a wrapper around the default haystack signal processor that
-    does nothing when run during tests or importing a fixture.
+    #This is just a wrapper around the default haystack signal processor that
+    #does nothing when run during tests or importing a fixture.
 
-    It has been "borrowed" from the ceda_moles_django app
-    """
+    #It has been "borrowed" from the ceda_moles_django app
+    
     def allow(self):
         # Hack to avoid re-indexing whilst a fixture is being imported (outside
         # the tests). Otherwise this seems to lead to problems where objects
@@ -40,7 +41,7 @@ class HaystackSignalProcessor(RealtimeSignalProcessor):
     def handle_delete(self, *args, **kwargs):
         if self.allow():
             super(HaystackSignalProcessor, self).handle_delete(*args, **kwargs)
-
+"""
 
 @receiver(post_save, sender=Document)
 def update_haystack_index(sender, **kwargs):
