95fc7f5620bbe395920b530ac24d1cb21bdf45d5
m-haines <matthew.r.haines@gmail.com>
2026-09-21 19:05:17 +0100

    Removed grant formatting if nerc or ukri ID is missing as I don't think it is needed for Github (it is for Jira).

--- datamad2/create_github_issue.py
+++ datamad2/create_github_issue.py
@@ -232,12 +232,12 @@ def make_github_issue(request, imported_grant) -> bool:
     print("Got the repository ID!", repository_id)
 
     if (imported_grant.nerc_id == "") & (imported_grant.ukri_id == ""):
-        if "/" in imported_grant.grant_ref:
-            nerc_id = imported_grant.grant_ref.replace('/', '\\u002f')
-            ukri_id = imported_grant.grant_ref.replace('/', '\\u002f')
-        else:
-            nerc_id = imported_grant.grant_ref
-            ukri_id = imported_grant.grant_ref
+        # if "/" in imported_grant.grant_ref:
+        #     nerc_id = imported_grant.grant_ref.replace('/', '\\u002f')
+        #     ukri_id = imported_grant.grant_ref.replace('/', '\\u002f')
+        # else:
+        nerc_id = imported_grant.grant_ref
+        ukri_id = imported_grant.grant_ref
     else:
         nerc_id = imported_grant.nerc_id
         ukri_id = imported_grant.ukri_id
