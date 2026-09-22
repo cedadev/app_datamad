b5963160a7f48bd0063b682ba5db4db96d18dfdb
m-haines <matthew.r.haines@gmail.com>
2026-09-22 14:30:31 +0100

    Fixed issue with user options only displaying Github. Now displays Jira options for non CEDA datacentres.

--- datamad2/views/github.py
+++ datamad2/views/github.py
@@ -93,6 +93,9 @@ def push_to_github(request, pk):
         return redirect('grant_detail', pk=pk)
 
     try:
+        lemon = request.user.github_username
+        lime = request.user.data_centre
+        pomelo = lime.name
         issue = make_github_issue(request, grant.importedgrant)
         link = issue["url"]
 
